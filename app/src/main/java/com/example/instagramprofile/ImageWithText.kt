218d4a8ab49ff51092abb1403032989d67f463a6
abhinay <abhinay212@gmail.com>
2025-01-12 01:35:22 +0530

    Profile UI completed

---
+++ app/src/main/java/com/example/instagramprofile/ImageWithText.kt
@@ -0,0 +1,14 @@
+package com.example.instagramprofile
+
+import androidx.compose.ui.graphics.painter.Painter
+
+
+/**
+ * Created by Abhinay on 12/01/25.
+ *
+ *
+ */
+data class ImageWithText(
+    val image: Painter,
+    val text: String
+)
